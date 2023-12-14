d0dbd819be98ef1777c32744a59426951ed38280
Tony Kalalian <120026287+tonykalalian@users.noreply.github.com>
2023-12-14 18:22:04 +0200

    AA

--- prompt.js
+++ prompt.js
@@ -4,8 +4,6 @@ const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@googl
 const dotenv = require("dotenv");
 const readline = require("readline");
 const path = require("path");
-const helmet = require("helmet");
-const cors = require("cors");
 
 dotenv.config();
 
@@ -14,12 +12,6 @@ const PORT = process.env.PORT || 3000;
 
 const MODEL_NAME = "gemini-pro";
 const API_KEY = process.env.API_KEY;
-
-// Validate required environment variables
-if (!API_KEY) {
-  console.error("API_KEY is not set. Exiting...");
-  process.exit(1);
-}
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -49,16 +41,7 @@ const chat = model.startChat({
   history: [],
 });
 
-app.use(helmet());
-
 app.use(bodyParser.json());
-
-app.use(cors());
-
-app.use((err, req, res, next) => {
-  console.error("Global Error Handler:", err);
-  res.status(500).json({ error: "Something went wrong" });
-});
 
 app.post("/send-message", async (req, res) => {
   const userInput = req.body.userInput;
@@ -95,7 +78,3 @@ function cleanup() {
 
 process.on("SIGINT", cleanup);
 process.on("SIGTERM", cleanup);
-
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled Rejection at:", promise, "reason:", reason);
-});
